e4ff2fac3f39612eff15c9186f8bf14d00fb70aa
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-06-22 01:15:08 +0200

    Error handling with containers

---
+++ errorhandling/task1.js
@@ -0,0 +1,96 @@
+// IMPLEMENT FILTER FOR MAYBE
+// RETURN NOTHING IF PREDICATE FAILS
+
+class Monad {
+    constructor(x) {
+        this.x = x;
+    }
+
+    map(fn) {
+        return new this.constructor(fn(this));
+    }
+
+    chain(fn) {
+        return this.map(fn).unwrap();
+    }
+
+    unwrap() {
+        return this.x instanceof Monad ? this.x.unwrap() : this.x;
+    }
+
+    toString() {
+        return `${this.constructor.name}(${this.x})`;
+    }
+}
+
+class Maybe extends Monad {
+    static of(x) {
+        return x === null || x === undefined
+            ? new Nothing()
+            : new Just()
+    }
+}
+
+class Just extends Maybe {
+    constructor(x) {
+        super(x);
+    }
+
+    map(fn) {
+        return Maybe.of(fn(this.x));
+    }
+
+    chain(fn) {
+        return fn(this.x);
+    }
+
+    filter(predicate) {
+        return predicate(this.x) ? this : new Nothing();
+    }
+
+    toString() {
+        return `Just(${this.x instanceof Monad ? this.x.toString : this.x})`;
+    }
+
+    isNothing() {
+        return false;
+    }
+}
+
+class Nothing extends Maybe {
+    constructor() {
+        super(null);
+    }
+
+    map(_) {
+        return this;
+    }
+
+    chain(_) {
+        return this;
+    }
+
+    filter(_) {
+        return this;
+    }
+
+    unwrap() {
+        return this;
+    }
+
+    toString() {
+        return "Nothing";
+    }
+
+    isNothing() {
+        return true;
+    }
+}
+
+const result1 = Maybe.of(10).filter(x => x > 5);
+const result2 = Maybe.of(2).filter(x => x > 5);
+const result3 = Maybe.of(null).filter(x => x > 5);
+
+console.log(result1.toString());
+console.log(result2.toString());
+console.log(result3.toString());
---
+++ errorhandling/task2.js
@@ -0,0 +1,98 @@
+// CREATE A FUNCTION SAFEPROP(OBJ, PROP)
+// THAT RETURNS A MAYBE OF THE PROPERTY
+
+class Monad {
+    constructor(x) {
+        this.x = x;
+    }
+
+    map(fn) {
+        return new this.constructor(fn(this.x));
+    }
+
+    chain(fn) {
+        return this.map(fn).unwrap();
+    }
+
+    unwrap() {
+        return this.x instanceof Monad ? this.x.unwrap() : this.x;
+    }
+
+    toString() {
+        return `${this.constructor.name}${this.x}`;
+    }
+}
+
+class Maybe extends Monad {
+    static of(x) {
+        return x === null || x === undefined
+            ? new Nothing()
+            : new Just(x);
+    }
+}
+
+class Just extends Maybe {
+    constructor(x) {
+        super(x);
+    }
+
+    map(fn) {
+        return Maybe.of(fn(this.x));
+    }
+
+    chain(fn) {
+        return fn(this.x);
+    }
+
+    filter(predicate) {
+        return predicate(this.x) ? this : new Nothing();
+    }
+
+    toString() {
+        return `Just(${this.x instanceof Monad ? this.x.toString() : this.x})`;
+    }
+
+    isNothing() {
+        return false;
+    }
+}
+
+class Nothing extends Maybe {
+    constructor(x) {
+        super(null);
+    }
+
+    map(_) {
+        return this;
+    }
+
+    chain(_) {
+        return this;
+    }
+
+    filter(_) {
+        return this;
+    }
+
+    unwrap() {
+        return this;
+    }
+
+    toString() {
+        return "Nothing";
+    }
+
+    isNothing() {
+        return true;
+    }
+}
+
+const safeProp = (obj, prop) => {
+    obj && Object.prototype.hasOwnProperty.call(obj, prop)
+        ? Maybe.of(obj[prop])
+        : new Nothing();
+}
+
+const user = { name: "Alice", age: 30 };
+
+console.log(safeProp(user, "name")).toString();
